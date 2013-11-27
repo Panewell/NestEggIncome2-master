e7d85d3a2f1e31098e346d5cba43867a259f8ff6
scheri <junescheri@Junes-MacBook-Pro.local>
2013-11-26 21:31:49 -0600

    keyboard to FU view controller
    have an error on the FU view contoller m file.

--- FixedUpdatesViewController.h
+++ FixedUpdatesViewController.h
@@ -9,21 +9,30 @@
 #import <UIKit/UIKit.h>
 
 @interface FixedUpdatesViewController : UIViewController
+@property (strong) NSManagedObject *loandb;
 
-@property (weak, nonatomic) IBOutlet UITextField *txtFUSource;
-@property (weak, nonatomic) IBOutlet UITextField *txtFUPayments;
+- (IBAction)btnSave:(id)sender;
+
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *btnSave;
+@property (strong, nonatomic) IBOutlet UITextField *txtFuSource;
+@property (strong, nonatomic) IBOutlet UITextField *txtFUAmount;
+@property (strong, nonatomic) IBOutlet UITextField *txtFUJan;
+@property (strong, nonatomic) IBOutlet UITextField *txtFUFeb;
+@property (strong, nonatomic) IBOutlet UITextField *txtFUMar;
+@property (strong, nonatomic) IBOutlet UITextField *txtFUApr;
+@property (strong, nonatomic) IBOutlet UITextField *txtFUMay;
+@property (strong, nonatomic) IBOutlet UITextField *txtFUJun;
+@property (strong, nonatomic) IBOutlet UITextField *txtFUJul;
+@property (strong, nonatomic) IBOutlet UITextField *txtFUAug;
+@property (strong, nonatomic) IBOutlet UITextField *txtFUSep;
+@property (strong, nonatomic) IBOutlet UITextField *txtFUOct;
+@property (strong, nonatomic) IBOutlet UITextField *txtFUNov;
+@property (strong, nonatomic) IBOutlet UITextField *txtFUDec;
+- (IBAction)btnCancel:(id)sender;
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollview;
+-(IBAction) doneEditing:(id) sender;
+
 @property (weak, nonatomic) IBOutlet UITextField *txtFUBalance;
 @property (weak, nonatomic) IBOutlet UITextField *txtFURate;
-@property (weak, nonatomic) IBOutlet UITextField *txtFUJan;
-@property (weak, nonatomic) IBOutlet UITextField *txtFUFeb;
-@property (weak, nonatomic) IBOutlet UITextField *txtFUMar;
-@property (weak, nonatomic) IBOutlet UITextField *txtFUApr;
-@property (weak, nonatomic) IBOutlet UITextField *txtFUMay;
-@property (weak, nonatomic) IBOutlet UITextField *txtFUJun;
-@property (weak, nonatomic) IBOutlet UITextField *txtFUJul;
-@property (weak, nonatomic) IBOutlet UITextField *txtFUAug;
-@property (weak, nonatomic) IBOutlet UITextField *txtFUSep;
-@property (weak, nonatomic) IBOutlet UITextField *txtFUOct;
-@property (weak, nonatomic) IBOutlet UITextField *txtFUNov;
-@property (weak, nonatomic) IBOutlet UITextField *txtFUDec;
+
 @end
--- FixedUpdatesViewController.m
+++ FixedUpdatesViewController.m
@@ -13,22 +13,7 @@
 @end
 
 @implementation FixedUpdatesViewController
-
-@synthesize txtFUSource;
-@synthesize txtFUPayments;
-@synthesize txtFUJan;
-@synthesize txtFUFeb;
-@synthesize txtFUMar;
-@synthesize txtFUApr;
-@synthesize txtFUMay;
-@synthesize txtFUJun;
-@synthesize txtFUJul;
-@synthesize txtFUAug;
-@synthesize txtFUSep;
-@synthesize txtFUOct;
-@synthesize txtFUNov;
-@synthesize txtFUDec;
-
+@synthesize loandb,Source,Amount,jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec,scrollview,btnSave;
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -38,16 +23,215 @@
     return self;
 }
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    
+    if (self.loandb) {
+        [self.Source setText:[self.loandb valueForKey:@"source"]];
+        [self.Amount setText:[self.loandb valueForKey:@"amount"]];
+        [self.jan setText:[self.loandb valueForKey:@"jan"]];
+        [self.feb setText:[self.loandb valueForKey:@"feb"]];
+        [self.mar setText:[self.loandb valueForKey:@"mar"]];
+        [self.apr setText:[self.loandb valueForKey:@"apr"]];
+        [self.may setText:[self.loandb valueForKey:@"may"]];
+        [self.jun setText:[self.loandb valueForKey:@"jun"]];
+        [self.jul setText:[self.loandb valueForKey:@"jan"]];
+        [self.aug setText:[self.loandb valueForKey:@"feb"]];
+        [self.sep setText:[self.loandb valueForKey:@"mar"]];
+        [self.oct setText:[self.loandb valueForKey:@"apr"]];
+        [self.nov setText:[self.loandb valueForKey:@"may"]];
+        [self.dec setText:[self.loandb valueForKey:@"jun"]];
+        [btnSave setTitle:@"Update" forState:UIControlStateNormal];
+        
+        
+    }
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
+                                   initWithTarget:self
+                                   action:@selector(dismissKeyboard)];
+    
+    [self.view addGestureRecognizer:tap];
+}
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    // Do any additional setup after loading the view.
+    // Do any additional setup after loading the view.
+    
+    
+    
+}
+-(void)dismissKeyboard {
+    [Source resignFirstResponder];
+    [Amount resignFirstResponder];
+    [jan resignFirstResponder];
+    [feb resignFirstResponder];
+    [mar resignFirstResponder];
+    [apr resignFirstResponder];
+    [may resignFirstResponder];
+    [jun resignFirstResponder];
+    [jul resignFirstResponder];
+    [aug resignFirstResponder];
+    [sep resignFirstResponder];
+    [oct resignFirstResponder];
+    [nov resignFirstResponder];
+    [dec resignFirstResponder];
+
+}
+-(IBAction) doneEditing:(id) sender {
+    [sender resignFirstResponder];
 }
 
+- (NSManagedObjectContext *)managedObjectContext {
+    NSManagedObjectContext *context = nil;
+    id delegate = [[UIApplication sharedApplication] delegate];
+    if ([delegate performSelector:@selector(managedObjectContext)]) {
+        context = [delegate managedObjectContext];
+    }
+    return context;
+    
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-@end
+- (IBAction)btnSave:(id)sender {
+    NSManagedObjectContext *context = [self managedObjectContext];
+    int mo;
+    NSString *OnorOff;
+    if (self.loandb) {
+        // Update existing device
+        
+        [self.loandb setValue:self.Source.text forKey:@"source"];
+        [self.loandb setValue:self.PayAmount.text forKey:@"amount"];
+        mo = [self.jan.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [self.loandb setValue:OnorOff forKey:@"jan"];
+        mo = [self.feb.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [self.loandb setValue:OnorOff forKey:@"feb"];
+        mo = [self.mar.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [self.loandb setValue:OnorOff forKey:@"mar"];
+        mo = [self.apr.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [self.loandb setValue:OnorOff forKey:@"apr"];
+        mo = [self.may.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [self.loandb setValue:OnorOff forKey:@"may"];
+        mo = [self.jun.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [self.loandb setValue:OnorOff forKey:@"jun"];
+        mo = [self.jul.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [self.loandb setValue:OnorOff forKey:@"jul"];
+        mo = [self.aug.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [self.loandb setValue:OnorOff forKey:@"aug"];
+        mo = [self.sep.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [self.loandb setValue:OnorOff forKey:@"sep"];
+        mo = [self.oct.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [self.loandb setValue:OnorOff forKey:@"oct"];  mo = [self.PayAmount.text doubleValue];
+        mo = [self.nov.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [self.loandb setValue:OnorOff forKey:@"nov"];
+        mo = [self.dec.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [self.loandb setValue:OnorOff forKey:@"dec"];
+        
+        //interestrate.text=[[NSString alloc] initWithFormat:@"@%f", total];
+    } else {
+        // Create a new device
+        NSManagedObject *newDevice = [NSEntityDescription insertNewObjectForEntityForName:@"Tblfix" inManagedObjectContext:context];
+        [newDevice setValue:self.Source.text forKey:@"source"];
+        [newDevice setValue:self.PayAmount.text forKey:@"amount"];
+        [newDevice setValue:self.jan.text forKey:@"jan"];
+        [newDevice setValue:self.feb.text forKey:@"feb"];
+        [newDevice setValue:self.mar.text forKey:@"mar"];
+        [newDevice setValue:self.apr.text forKey:@"apr"];
+        [newDevice setValue:self.may.text forKey:@"may"];
+        [newDevice setValue:self.jun.text forKey:@"jun"];
+        [newDevice setValue:self.jul.text forKey:@"jul"];
+        [newDevice setValue:self.aug.text forKey:@"aug"];
+        [newDevice setValue:self.sep.text forKey:@"sep"];
+        [newDevice setValue:self.oct.text forKey:@"oct"];
+        [newDevice setValue:self.nov.text forKey:@"nov"];
+        [newDevice setValue:self.dec.text forKey:@"dec"];
+        mo = [self.jan.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [newDevice setValue:OnorOff forKey:@"jan"];
+        mo = [self.feb.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [newDevice setValue:OnorOff forKey:@"feb"];
+        mo = [self.mar.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [newDevice setValue:OnorOff forKey:@"mar"];
+        mo = [self.apr.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [newDevice setValue:OnorOff forKey:@"apr"];
+        mo = [self.may.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [newDevice setValue:OnorOff forKey:@"may"];
+        mo = [self.jun.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [newDevice setValue:OnorOff forKey:@"jun"];
+        mo = [self.jul.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [newDevice setValue:OnorOff forKey:@"jul"];
+        mo = [self.aug.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [newDevice setValue:OnorOff forKey:@"aug"];
+        mo = [self.sep.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [newDevice setValue:OnorOff forKey:@"sep"];
+        mo = [self.oct.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [newDevice setValue:OnorOff forKey:@"oct"];  mo = [self.PayAmount.text doubleValue];
+        mo = [self.nov.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [newDevice setValue:OnorOff forKey:@"nov"];
+        mo = [self.dec.text doubleValue];
+        OnorOff = [[NSString alloc] initWithFormat:@"%d", mo];
+        [newDevice setValue:OnorOff forKey:@"dec"];
+        
+        
+    }
+    NSError *error = nil;
+    // Save the object to persistent store
+    if (![context save:&error]) {
+        NSLog(@"Can't Save! %@ %@", error, [error localizedDescription]);
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)btnCancel:(id)sender:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    CGPoint scrollPoint = CGPointMake(0, textField.frame.origin.y);
+    [scrollview setContentOffset:scrollPoint animated:YES];
+}
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    [scrollview setContentOffset:CGPointZero animated:YES];
+}
+- (void)textViewDidBeginEditing:(UITextView *)textView {
+    CGPoint scrollPoint = CGPointMake(0, textView.frame.origin.y);
+    [scrollview setContentOffset:scrollPoint animated:YES];
+}
+- (void)textViewDidEndEditing:(UITextView *)textView {
+    [scrollview setContentOffset:CGPointZero animated:YES];
+}
+
+    
+    
+    
+    
+    
+    @end
